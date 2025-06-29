7d5699ebc3327e120614f7dd53ab8b0cb79afc96
pritha10 <140228023+pritha10@users.noreply.github.com>
2025-06-29 14:17:14 +0530

    NUnit and MOQ

---
+++ week2/module2/emailunittest/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using Moq;
+using CustomerCommLib;
+
+namespace CustomerCommLib.Tests
+{
+    [TestFixture]
+    public class CustomerCommTests
+    {
+        [Test]
+        public void SendMailToCustomer_WhenCalled_ReturnsTrue()
+        {
+            // Arrange
+            var mockMailSender = new Mock<IMailSender>();
+            mockMailSender
+                .Setup(x => x.SendMail(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(true);
+
+            var customerComm = new CustomerComm(mockMailSender.Object);
+
+            // Act
+            var result = customerComm.SendMailToCustomer();
+
+            // Assert
+            NUnit.Framework.Assert.IsTrue(result);
+
+            mockMailSender.Verify(x => x.SendMail(It.IsAny<string>(), It.IsAny<string>()), Times.Once);
+        }
+    }
+}
