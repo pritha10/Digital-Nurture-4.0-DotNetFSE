66b84e4066e81e1ca0d181d7bce708e8b81bfab5
pritha10 <140228023+pritha10@users.noreply.github.com>
2025-07-05 15:22:18 +0530

    EF

---
+++ week3/RetailInventory_Lab1/Migrations/CInventoryContextModelSnapshot.cs
@@ -0,0 +1,50 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using RetailInventory;
+
+#nullable disable
+
+namespace RetailInventory.Migrations
+{
+    [DbContext(typeof(InventoryContext))]
+    partial class InventoryContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+#pragma warning disable 612, 618
+            modelBuilder
+                .HasAnnotation("ProductVersion", "9.0.6")
+                .HasAnnotation("Relational:MaxIdentifierLength", 128);
+
+            SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
+
+            modelBuilder.Entity("RetailInventory.Models.Product", b =>
+                {
+                    b.Property<int>("ProductId")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("int");
+
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("ProductId"));
+
+                    b.Property<string>("Category")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<int>("StockQuantity")
+                        .HasColumnType("int");
+
+                    b.HasKey("ProductId");
+
+                    b.ToTable("Products");
+                });
+#pragma warning restore 612, 618
+        }
+    }
+}
---
+++ week3/RetailInventory_Lab1/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace RetailInventory.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }    
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public int StockQuantity { get; set; }
+    }
+}
