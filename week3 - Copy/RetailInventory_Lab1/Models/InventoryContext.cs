9dc979096344e444838e6f1aa962bdf4a03ead80
pritha10 <140228023+pritha10@users.noreply.github.com>
2025-07-05 20:03:07 +0530

    EF

---
+++ week3 - Copy/RetailInventory_Lab1/Models/InventoryContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RetailInventory.Models;
+
+namespace RetailInventory
+{
+    public class InventoryContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=RetailInventoryDB;Trusted_Connection=True;");
+        }
+    }
+}
