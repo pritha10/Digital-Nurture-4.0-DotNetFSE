8112695dd2fd7be634f2536f9518ace86e6be985
pritha10 <140228023+pritha10@users.noreply.github.com>
2025-07-06 14:55:45 +0530

    Lab 4

---
+++ week3/lab4/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Lab3.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        public List<Product> Products { get; set; } = new();
+    }
+}
