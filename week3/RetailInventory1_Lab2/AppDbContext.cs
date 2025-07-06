7e7854cadae561865b4852515cdfe3e08871dbe8
pritha10 <140228023+pritha10@users.noreply.github.com>
2025-07-06 11:24:57 +0530

    Lab 2

---
+++ week3/RetailInventory1_Lab2/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RetailInventory1.Models;
+
+
+namespace RetailInventory1
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=RetailDb;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ week3/RetailInventory1_Lab2/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace RetailInventory1.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+        public List<Product> Products { get; set; }
+    }
+}
