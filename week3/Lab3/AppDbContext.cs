5eefa0b2dca6ddf6183a17b942b001550be7f313
pritha10 <140228023+pritha10@users.noreply.github.com>
2025-07-06 12:50:09 +0530

    Lab3

---
+++ week3/Lab3/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Lab3.Models;
+
+namespace Lab3
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=RetailLab3Db;Trusted_Connection=True;");
+        }
+    }
+}
