0bea32c29f0e1f87b8cfe4c1e54e04bd7a87d56c
pritha10 <140228023+pritha10@users.noreply.github.com>
2025-07-19 12:33:28 +0530

    Week5

---
+++ week5/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtAuthDemo2.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet("secure-data")]
+        public IActionResult GetSecureData()
+        {
+            return Ok("You have accessed a protected endpoint!");
+        }
+    }
+}
