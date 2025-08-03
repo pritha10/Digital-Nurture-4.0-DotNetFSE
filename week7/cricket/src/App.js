058ed67a1690019425fcda556507c7d24b12b0c5
pritha10 <140228023+pritha10@users.noreply.github.com>
2025-08-03 10:42:27 +0530

    week7

---
+++ week7/cricket/src/App.js
@@ -0,0 +1,17 @@
+﻿// src/App.js
+import React from 'react';
+import ListofPlayers from './components/ListofPlayers';
+import IndianPlayers from './components/IndianPlayers';
+
+function App() {
+    const flag = true; // Toggle between true/false to display components conditionally
+
+    return (
+        <div className="App">
+            <h1>🏏 Cricket App</h1>
+            {flag ? <ListofPlayers /> : <IndianPlayers />}
+        </div>
+    );
+}
+
+export default App;
